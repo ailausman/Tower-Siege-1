4fb9ba9c68a9d62385e38f262803507b15916199
ailausman <86518850+ailausman@users.noreply.github.com>
2021-12-16 18:55:38 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,92 +4,96 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var ball,ground;
-var stand1,stand2;
+var ball, ground;
+var stand1, stand2;
 var slingShot;
 var rock_img;
 
-function preload(){
-  this.rock=loadImage("rock.png");
+function preload() {
+  this.rock = loadImage("rock.png");
 
 }
+
 function setup() {
 
-  createCanvas(900,400);
+  createCanvas(900, 400);
   engine = Engine.create();
   world = engine.world;
   Engine.run(engine);
-  
+
   ground = new Ground();
-  stand1 = new Stand(390,300,250,10);
-  stand2 = new Stand(700,200,200,10);
- 
+  stand1 = new Stand(390, 300, 250, 10);
+  stand2 = new Stand(700, 200, 200, 10);
+
   //level one
-  block1 = new Block(300,275,30,40);
-  block2 = new Block(330,275,30,40);
-  block3 = new Block(360,275,30,40);
-  block4 = new Block(390,275,30,40);
-  block5 = new Block(420,275,30,40);
-  block6 = new Block(450,275,30,40);
-  block7 = new Block(480,275,30,40);
+  block1 = new Block(300, 275, 30, 40);
+  block2 = new Block(330, 275, 30, 40);
+  block3 = new Block(360, 275, 30, 40);
+  block4 = new Block(390, 275, 30, 40);
+  block5 = new Block(420, 275, 30, 40);
+  block6 = new Block(450, 275, 30, 40);
+  block7 = new Block(480, 275, 30, 40);
   //level two
-  block8 = new Block(330,235,30,40);
-  block9 = new Block(360,235,30,40);
-  block10 = new Block(390,235,30,40);
-  block11 = new Block(420,235,30,40);
-  block12 = new Block(450,235,30,40);
+  block8 = new Block(330, 235, 30, 40);
+  block9 = new Block(360, 235, 30, 40);
+  block10 = new Block(390, 235, 30, 40);
+  block11 = new Block(420, 235, 30, 40);
+  block12 = new Block(450, 235, 30, 40);
   //level three
-  block13 = new Block(360,195,30,40);
-  block14 = new Block(390,195,30,40);
-  block15 = new Block(420,195,30,40);
+  block13 = new Block(360, 195, 30, 40);
+  block14 = new Block(390, 195, 30, 40);
+  block15 = new Block(420, 195, 30, 40);
   //top
-  block16 = new Block(390,155,30,40);
+  block16 = new Block(390, 155, 30, 40);
 
   //set 2 for second stand
   //level one
-  blocks1 = new Block(640,175,30,40);
-  blocks2 = new Block(670,175,30,40);
-  blocks3 = new Block(700,175,30,40);
-  blocks4 = new Block(730,175,30,40);
-  blocks5 = new Block(760,175,30,40);
+  blocks1 = new Block(640, 175, 30, 40);
+  blocks2 = new Block(670, 175, 30, 40);
+  blocks3 = new Block(700, 175, 30, 40);
+  blocks4 = new Block(730, 175, 30, 40);
+  blocks5 = new Block(760, 175, 30, 40);
   //level two
-  blocks6 = new Block(670,135,30,40);
-  blocks7 = new Block(700,135,30,40);
-  blocks8 = new Block(730,135,30,40);
+  blocks6 = new Block(670, 135, 30, 40);
+  blocks7 = new Block(700, 135, 30, 40);
+  blocks8 = new Block(730, 135, 30, 40);
   //top
-  blocks9 = new Block(700,95,30,40);
+  blocks9 = new Block(700, 95, 30, 40);
 
   //ball holder with slings
-  ball = Bodies.circle(50,200,20);
-  World.add(world,ball);
+  ball = Bodies.circle(50, 200, 20);
+  World.add(world, ball);
 
-  slingShot = new Slingshot(this.ball,{x:100,y:200});
+  slingShot = new Slingshot(this.ball, {
+    x: 100,
+    y: 200
+  });
 
 }
 
 function draw() {
-  background(56,44,44); 
- 
+  background(56, 44, 44);
+
   imageMode(CENTER);
-    image(this.rock,100,200,50,50);
-    
- 
-  
+  image(this.rock, 100, 200, 50, 50);
 
 
-  stroke(0,0,0);
+
+
+
+  stroke(0, 0, 0);
   fill("white");
   textSize(20);
   fill("lightyellow");
-  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks", 100, 30);
 
   ground.display();
   stand1.display();
   stand2.display();
 
   strokeWeight(2);
-  stroke(0,0,0);
-  
+  stroke(0, 0, 0);
+
   fill("skyblue");
   block1.display();
   block2.display();
@@ -125,12 +129,17 @@ function draw() {
   fill("gold");
 
 
-  
+
   slingShot.display();
 }
-function mouseDragged(){
-  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+
+function mouseDragged() {
+  Matter.Body.setPosition(this.ball, {
+    x: mouseX,
+    y: mouseY
+  });
 }
-function mouseReleased(){
+
+function mouseReleased() {
   slingShot.fly();
 }
